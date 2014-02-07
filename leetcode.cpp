0ad3900084af802453632e3b46eaa87f972807da
arthur1026 <chenarthur1026@gmail.com>
2014-02-06 21:26:18 -0500

    add isSameTree()
    Same Tree

--- leetcode.cpp
+++ leetcode.cpp
@@ -1750,6 +1750,22 @@ public:
                 matrix[r][c] = tmp;
         }
     }
+
+    /* Same Tree
+     Given two binary trees, write a function to check if they are equal or not.
+     
+     Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+     */
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (!p && !q)
+            return true;
+        if (p && q)
+            if (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
+                return true;
+        
+        return false;
+    }
+
 };
 
 template <class T>
