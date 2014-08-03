d97ae55fa475fa8652485ab21e1a95e72c93cc94
Lu Chen <chenarthur1026@gmail.com>
2014-08-02 20:32:20 -0700

    add fourSum()

--- leetcode.cpp
+++ leetcode.cpp
@@ -52,6 +52,67 @@ public:
 
 class Solution {
 public:
+/* 4Sum 
+Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Note:
+Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+The solution set must not contain duplicate quadruplets.
+    For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+
+    A solution set is:
+    (-1,  0, 0, 1)
+    (-2, -1, 1, 2)
+    (-2,  0, 0, 2)
+*/
+    vector<vector<int> > fourSum(vector<int> &num, int target) {
+        vector<vector<int> > results;
+        if (num.size() < 4)
+            return results;
+        sort(num.begin(), num.end());
+            
+        for (int i = 0; i < num.size() - 3; i++) {
+            if (i > 0 && num[i] == num[i-1])
+                continue;
+            for (int j = i + 1; j < num.size() - 2; j++) {
+                if (j > i+1 && num[j] == num[j-1])
+                    continue;
+                    
+                // become 2-sum problem
+                int left = j+1;
+                int right = num.size() -1;
+                int sumij = num[i] + num[j];
+                while (left < right) {
+                    int sum = sumij + num[left] + num[right];
+                    if (sum < target)
+                        left++;
+                    else if (sum > target)
+                        right--;
+                    else {  // found a match
+                        vector<int> single_result;
+                        single_result.push_back(num[i]);
+                        single_result.push_back(num[j]);
+                        single_result.push_back(num[left]);
+                        single_result.push_back(num[right]);
+                        
+                        results.push_back(single_result);
+                        
+                        // look for another possible pair of two numbers that give the same sum
+                        left++;
+                        right--;
+                        
+                        while (left < right && num[left] == num[left-1])
+                            left++;
+                        while (left < right && num[right] == num[right+1])
+                            right--;
+                    }
+                }
+            }
+        }
+        
+        return results;
+    }
+    
 /* Merge k Sorted Lists 
 Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
 */
