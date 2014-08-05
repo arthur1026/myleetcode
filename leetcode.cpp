27f4e38e517770e940229d8fbd7a2f8091506667
Lu Chen <chenarthur1026@gmail.com>
2014-08-05 14:22:09 -0700

    Solved "Largest Rectangle in Histogram"

--- leetcode.cpp
+++ leetcode.cpp
@@ -52,6 +52,37 @@ public:
 
 class Solution {
 public:
+/*Largest Rectangle in Histogram 
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+
+
+Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+
+
+The largest rectangle is shown in the shaded area, which has area = 10 unit.
+
+For example,
+Given height = [2,1,5,6,2,3],
+return 10.
+*/
+    int largestRectangleArea(vector<int> &height) {
+        // for each bar, check the maximum area it could cover
+        int maxarea = 0;
+        stack<int> s;
+        height.push_back(0);
+        for (int i = 0; i < height.size();) {
+            if (s.empty() || height[i] > height[s.top()])
+                s.push(i++);
+            else {
+                int tmp = s.top();
+                s.pop();
+                maxarea = max(maxarea, height[tmp] * (s.empty() ? i : i - s.top() - 1));
+            }
+        }
+
+        return maxarea;
+    }
+    
 /*Length of Last Word 
 Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
 
