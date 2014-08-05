dabdfedf7e7bce427a17a75a0133cd9159ca6880
Lu Chen <chenarthur1026@gmail.com>
2014-08-05 16:55:33 -0700

    Solved "Gray Code"

--- leetcode.cpp
+++ leetcode.cpp
@@ -52,6 +52,36 @@ public:
 
 class Solution {
 public:
+/*Gray Code
+The gray code is a binary numeral system where two successive values differ in only one bit.
+
+Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+
+For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+Note:
+For a given n, a gray code sequence is not uniquely defined.
+
+For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+
+For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+*/
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        const size_t size = 1 << n;
+        result.reserve(size);
+        
+        for (size_t i = 0; i < size; i++) {
+            result.push_back( i ^ (i >> 1) );
+        }
+        
+        return result;
+    }
+    
 /*Reverse Nodes in k-Group 
 Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
 
