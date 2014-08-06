53635a2e638224ac62ec1ebdbe4ebc40915de831
Lu Chen <chenarthur1026@gmail.com>
2014-08-05 23:37:29 -0700

    Solved "Maximal Rectangle"

--- leetcode.cpp
+++ leetcode.cpp
@@ -52,6 +52,47 @@ public:
 
 class Solution {
 public:
+/*Maximal Rectangle 
+Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all ones and return its area.
+*/
+    int maximalRectangle(vector<vector<char> > &matrix) {
+        if (matrix.empty()) return 0;
+        
+        const int m = matrix.size();
+        const int n = matrix[0].size();
+        vector<int> H(n, 0);
+        vector<int> L(n, 0);
+        vector<int> R(n, n);
+        
+        int ret = 0;
+        for (int i = 0; i < m; i++) {
+            int left = 0 ;
+            int right = n;
+            for (int j=0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    ++H[j];
+                    L[j] = max(L[j], left);
+                } else {
+                    left = j+1;
+                    H[j] = 0;
+                    L[j] = 0;
+                    R[j] = n;
+                }
+            }
+            
+            for (int j = n-1; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    R[j] = min(R[j], right);
+                    ret = max(ret, H[j] * (R[j] - L[j]));
+                } else {
+                    right = j;
+                }
+            }
+        }
+        
+        return ret;
+    }
+    
 /*Wildcard Matching 
 Implement wildcard pattern matching with support for '?' and '*'.
 
