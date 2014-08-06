047701decb38f73c7453ac82f31074639a09c5cd
Lu Chen <chenarthur1026@gmail.com>
2014-08-05 22:28:12 -0700

    Solved "Substring with Concatenation of All Words"

--- leetcode.cpp
+++ leetcode.cpp
@@ -52,6 +52,43 @@ public:
 
 class Solution {
 public:
+/*Substring with Concatenation of All Words 
+You are given a string, S, and a list of words, L, that are all of the same length. Find all starting indices of substring(s) in S that is a concatenation of each word in L exactly once and without any intervening characters.
+
+For example, given:
+S: "barfoothefoobarman"
+L: ["foo", "bar"]
+
+You should return the indices: [0,9].
+(order does not matter).
+*/
+    vector<int> findSubstring(string s, vector<string> &dict) {
+        vector<int> results;
+        size_t wordLength = dict.front().length();
+        size_t catLength = wordLength * dict.size();
+
+        if (s.length() < catLength) return results;
+        
+        unordered_map<string, int> wordCount;
+        for (auto const& word : dict) ++wordCount[word];
+        
+        for (auto i = begin(s); i <= prev(end(s), catLength); ++i) {
+            unordered_map<string, int> unused(wordCount);
+            
+            for (auto j = i; j != next(i, catLength); j += wordLength) {
+                auto pos = unused.find(string(j, next(j, wordLength)));
+                
+                if (pos == unused.end() || pos->second == 0) break;
+                
+                if (--pos->second == 0) unused.erase(pos);
+            }
+            
+            if (unused.size() == 0) results.push_back(distance(begin(s), i));
+        }
+        
+        return results;
+    }
+    
 /*Scramble String 
 Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
 
